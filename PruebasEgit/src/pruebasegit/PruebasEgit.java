67dca6a9476ab959a2d9e90e68c413261b530f62
Carlos <tarea2git@gmail.com>
2024-02-12 09:55:44 +0100

    Tercer Commit máster

--- PruebasEgit/src/pruebasegit/PruebasEgit.java
+++ PruebasEgit/src/pruebasegit/PruebasEgit.java
@@ -6,6 +6,7 @@ public class PruebasEgit {
 		System.out.println("PruebasEgit");
 		System.out.println("Segundo commit máster");
 		System.out.println("Primer Commit ramma_1");
+		System.out.println("Tercer Commit Máster");
 	}
 
 }
